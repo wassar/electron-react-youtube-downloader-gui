a83bf272b0c11ce6c0db14f7b97b451b6db4d7a2
Wassar@MBP <wassar@msn.com>
2022-05-09 21:14:41 +0100

    fix build db file

--- src/components/ui/download-card/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Card } from "@mui/material";
-
-interface DownloadCardProps {}
-
-const DownloadCard: React.FC<DownloadCardProps> = () => {
-    return <Card></Card>;
-};
-
-export default DownloadCard;
